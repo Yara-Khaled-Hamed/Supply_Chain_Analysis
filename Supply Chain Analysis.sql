973604081b32fd8f7f8ad2a07615ce49a349d593
Yara Khaled <yarakhaled.te.eg@gmail.com>
2025-05-18 12:03:39 +0300

    Add files via upload

--- Supply Chain Analysis.sql
+++ Supply Chain Analysis.sql
@@ -1,3 +1,26 @@
+-- How does profit change with different discount levels?
+-- Less Discount means more profit
+-- Optimizes discount policies to maximize profit per sale.
+
+WITH DiscountBuckets AS (
+    SELECT *,
+        CASE 
+            WHEN [Order Item Discount Rate] < 0.10 THEN '0-10%'
+            WHEN [Order Item Discount Rate] between .10 and 0.20 THEN '10-20%'
+            WHEN [Order Item Discount Rate]between .20 and 0.25 THEN '10-25%'
+            ELSE '>25%' 
+        END AS Discount_Range
+    FROM Supply_Chain
+)
+SELECT 
+    Discount_Range,
+    COUNT(*) AS Order_Count,
+    ROUND(AVG([Order Profit Per Order]), 2) AS Avg_Profit
+FROM DiscountBuckets
+GROUP BY Discount_Range
+ORDER BY Avg_Profit desc;
+
+---------------------
 -- How does the average shipping duration vary by delivery status?
 -- Helps identify delays by delivery type to improve fulfillment efficiency.
 
@@ -10,7 +33,7 @@ group by [Delivery Status]
 -- What is the most used method?
 -- Found the most used method is DEBIT
 
-select type , count([customer city]) Count_of_orders, round(SUM(sales),2) as Total_Sales
+select type , count([customer city]) Count_of_orders, round(SUM(sales-[Order Item Discount]),2) as Total_Sales
 from Supply_Chain
 group by Type
 order by count_of_orders desc
@@ -84,29 +107,7 @@ order by Total_Profit desc
 
 --------------------------------------------
 
--- How does profit change with different discount levels?
--- Less Discount means more profit
--- Optimizes discount policies to maximize profit per sale.
 
-WITH DiscountBuckets AS (
-    SELECT *,
-        CASE 
-            WHEN [Order Item Discount Rate] < 0.10 THEN '0-10%'
-            WHEN [Order Item Discount Rate] between .10 and 0.20 THEN '10-20%'
-            WHEN [Order Item Discount Rate]between .20 and 0.25 THEN '10-25%'
-            ELSE '>25%' 
-        END AS Discount_Range
-    FROM Supply_Chain
-)
-SELECT 
-    Discount_Range,
-    COUNT(*) AS Order_Count,
-    ROUND(AVG([Order Profit Per Order]), 2) AS Avg_Profit
-FROM DiscountBuckets
-GROUP BY Discount_Range
-ORDER BY Avg_Profit desc;
-
----------------------
 
 -- Which segment gives the most profit per discount unit?
 -- Finds the most efficient segments for discount spending.
@@ -122,7 +123,7 @@ order by 'Profit per 1 unit of discount' desc
 -- Which cities drive the highest sales?
 -- Identifies high-value cities for regional targeting and distribution.
 
-select top(5) [Customer City], round(sum(Sales),2) Total
+select top(5) [Customer City], round(sum(Sales-[Order Item Discount]),2) Total
 from Supply_Chain
 group by [Customer City]
 order by Total desc
@@ -136,7 +137,7 @@ WHERE [order City] IN (
     select top(5) [Customer City]
     from Supply_Chain
     group by [Customer City]
-    order by round(sum(Sales),2) desc
+    order by round(sum(Sales-[Order Item Discount]),2) desc
 );
 
 --------------------
@@ -144,7 +145,7 @@ WHERE [order City] IN (
 -- Who are the top high-spending customers? 
 -- Supports VIP customer retention strategies and loyalty programs.
 
-select [Customer Full Name], round(sum(Sales),0) Sales
+select [Customer Full Name], round(sum(Sales-[Order Item Discount]),0) Sales
 from Supply_Chain
 group by [Customer Full Name]
 order by Sales desc
@@ -152,7 +153,7 @@ order by Sales desc
 -- Is "Mary Smith" one customer or multiple people?
 -- Found "Mary Smith" is an outlier, possibly due to multiple identities across cities
 -- Mary Smith from Caguas is the highest one
-select [Customer Full Name], [Customer City], round(sum(Sales),0) Sales
+select [Customer Full Name], [Customer City], round(sum(Sales-[Order Item Discount]),0) Sales
 from Supply_Chain
 where [Customer Full Name] = 'Mary Smith'
 group by [Customer Full Name], [Customer City]
@@ -182,3 +183,10 @@ FROM Supply_Chain
 group by [Product Name], [Category Name], [Product Price]
 order by Total_Orders desc
 
+-- What is the lowes ordered products to make offers on them 
+
+SELECT [Product Name],
+       count(*)Total_Orders
+FROM Supply_Chain
+group by [Product Name]
+order by Total_Orders asc
